a252737ada2af419c748546e75380c6f11089647
Ryan McFadden <ryanpmcfadden@gmail.com>
2015-09-23 09:06:41 -0700

    First commit

---
+++ memoirs.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Plugin Name: Memoirs
+* Plugin URI: https://github.com/rmcfadden/memoirs
+* Description: A brief description about your plugin.
+* Version: 1.0
+* Author: Ryan McFadden
+* Author URI: https://github.com/rmcfadden
+* License: GPLv2
+*/
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
